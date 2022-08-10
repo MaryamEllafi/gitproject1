10aabdd85075612288773148abcf956224baab04
MaryamEllafi <mariamellafisav@gmail.com>
2022-08-10 13:04:09 +0200

    test git project1

---
+++ src/main/java/waha/mycompany/gitproject1/Gitproject1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package waha.mycompany.gitproject1;
+
+import java.time.Year;
+
+/**
+ *
+ * @author almadkhal2
+ */
+public class Gitproject1 {
+
+    public static void main(String[] args) {
+      //System.out.println(" ****************");
+        int a=10,  R;
+        String [] array1 = {"me" , "he" , "she"};
+       /* for (int i = 1; i < 21; i++) 
+        {
+            System.out.print((i+i+1) + "  ");
+        }
+        System.out.println();
+        System.out.println("*******************************");
+        /*/
+       for (String y :array1)
+       {
+           System.out.println("  " + y);
+        }  System.out.println(array1.length);
+        
+        
+        System.out.println(" w45645634545   wdqfwsfew");
+        
+    }
+}
