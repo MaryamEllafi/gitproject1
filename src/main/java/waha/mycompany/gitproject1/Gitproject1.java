3323184a96751ab076306d18ccec946d731b206e
MaryamEllafi <mariamellafisav@gmail.com>
2022-08-11 11:51:11 +0200

    new changes

--- src/main/java/waha/mycompany/gitproject1/Gitproject1.java
+++ src/main/java/waha/mycompany/gitproject1/Gitproject1.java
@@ -24,13 +24,38 @@ public class Gitproject1 {
         System.out.println();
         System.out.println("*******************************");
         /*/
-       for (String y :array1)
+      /* for (String y :array1)
        {
            System.out.println("  " + y);
         }  System.out.println(array1.length);
         
+       
+        System.out.println(" w4564563cvffdfffdfd4545   wdqfwsfew");*/
+             
+      
+       Test test = new Test();
+      test.sumxy(3,4);
         
-        System.out.println(" w45645634545   wdqfwsfew");
+      
+     /* for (int i = 1; i <= 4; i++) {
+			// Inner loop for iteration
+			for (int j = 1; j <= 3; j++) {
+				if (i == 3 && j == 2) {
+					// Continue statement in inner loop to
+					// skip the execution when i==3 and j==2
+					continue;
+				}
+				// Print elements to showcase keyword affect
+				System.out.println(i + " * " + j);
+			}   
+                   }*/
+     Gitproject1 aa = new Gitproject1();
         
+        System.out.println("result multixy" + aa.multixy(5, 4));
     }
+    int multixy(int x , int y)
+            {
+                return(x*y);
+}
+    
 }
--- src/main/java/waha/mycompany/gitproject1/Test.java
+++ src/main/java/waha/mycompany/gitproject1/Test.java
@@ -1,12 +1,14 @@
+
+package waha.mycompany.gitproject1;
+
 public class Test {
-   public void pupAge() {
-      int age = 0;
-      age = age + 7;
-      System.out.println("Puppy age is : " + age);
+   public void sumxy( int x, int y) {
+      //int age = 0;
+      //age = age + 7;
+      System.out.println("sum : " + (x+y));
    }
 
-   public static void main(String args[]) {
-      Test test = new Test();
-      test.pupAge();
-   }
+    
+      
+    
 }
